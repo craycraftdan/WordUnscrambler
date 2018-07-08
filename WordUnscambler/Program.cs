048e5a11922006ec9c17830922157ca3a7193b74
dan.craycraft@lids.com <dan.craycraft@lids.com>
2018-07-08 17:06:51 -0400

    addng notes

--- WordUnscambler/Program.cs
+++ WordUnscambler/Program.cs
@@ -10,6 +10,47 @@ namespace WordUnscambler
     {
         static void Main(string[] args)
         {
+
+
+            //Loops
+            //for(int i=1; i<=5; i++)
+            //{
+            //    Console.WriteLine("This is line {0}", i);
+            //}
+            //var input = Console.ReadLine();
+            //Console.WriteLine("User input is {0}", input);
+            //while(!input.Equals(string.Empty))
+            //{
+            //    input = Console.ReadLine();
+            //    Console.WriteLine("Please enter some text");
+            //}
+            
+
+            //same as var
+            //int[] myArray = new int[5];
+            //myArray[0] = 7;
+            //myArray[1] = 8;
+            //myArray[2] = 4;
+            //myArray[3] = 5;
+            //myArray[4] = 15;
+            //Instead of doing it this way you can instantiate a array a lot faster this way
+            //int[] myArray = { 7, 8, 4, 5, 15 };
+            //Array.Sort(myArray);
+
+            //Arrays have inital capacity and can be used to make list, they're kind of the same.
+            //var myFirstArray = new int[10];
+            //var mySecondList = new List<int>();
+            //myFirstArray[9] = 5;
+            //Console.WriteLine(myFirstArray[9]);
+            //mySecondList.Add(2);
+            //Console.WriteLine(mySecondList[0]);
+
+            //List<int> myFirstList = new List<int>();  you can use List<int> or var
+            //var myFirstList = new List<int>();
+            //Console.WriteLine(myFirstList.Count());
+            //myFirstList.Add(10);
+            //myFirstList.Add(5);
+            //Console.WriteLine(myFirstList.Count());
         }
     }
 }
